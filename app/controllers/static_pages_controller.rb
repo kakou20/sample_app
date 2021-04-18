6be63661333a514be8c2d6f7577df636525a857e
Kou Ka <a2013hh@aiit.ac.jp>
2021-04-18 11:59:23 +0000

    Exec: rails generate controller StaticPages home help

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,9 @@
+class StaticPagesController < ApplicationController
+  def home
+    #=>"app/vies/#{リソース名}/@{アクション名}.html.erb"
+    #=>"app/view/statis_pages_controller.rb"
+  end
+
+  def help
+  end
+end
