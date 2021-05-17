6f76d545f1e5d70a4329dc9275c0fdff06660ca4
Kou Ka <a2013hh@aiit.ac.jp>
2021-05-17 14:44:45 +0000

    Finish ch06

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
-  def new
+
+  #GET /users/:id
+  def show
+   # @user = User.find(params[:id])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_141342) do
+ActiveRecord::Schema.define(version: 2021_05_17_133859) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
