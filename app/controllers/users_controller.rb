b61db054496b2fbd8d8bb96af20cbebbb7ba3016
Kou Ka <a2013hh@aiit.ac.jp>
2021-05-19 12:57:50 +0000

    Finish ch07

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,30 @@
 class UsersController < ApplicationController
-
-  #GET /users/:id
+  # GET /users/:id
   def show
-   # @user = User.find(params[:id])
+    @user = User.find(params[:id])
+  end
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # POST /users (+ params)
+  def create
+    # User.create(params[:user])
+    @user =  User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+      # Success (valid params)
+    else
+      # Failure (not valid params)
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
