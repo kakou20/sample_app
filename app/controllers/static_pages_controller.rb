20cf96407c7253457649373e247699553b62faf1
Kou Ka <a2013hh@aiit.ac.jp>
2021-05-30 16:10:27 +0000

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     #=>"app/vies/#{リソース名}/@{アクション名}.html.erb"
     #=>"app/view/statis_pages_controller.rb"
+    if logged_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
--- test/controllers/microposts_controller_test.rb
+++ test/controllers/microposts_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class MicropostsControllerTest < ActionDispatch::IntegrationTest
+
   def setup
     @micropost = microposts(:orange)
   end
@@ -9,7 +10,7 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
     assert_no_difference 'Micropost.count' do
       post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
     end
-    # assert_redirected_to login_url
+    assert_redirected_to login_url
   end
 
   test "should redirect destroy when not logged in" do
@@ -17,5 +18,14 @@ class MicropostsControllerTest < ActionDispatch::IntegrationTest
       delete micropost_path(@micropost)
     end
     assert_redirected_to login_url
+  end
+
+  test "should redirect destroy for wrong micropost" do
+    log_in_as(users(:michael))
+    micropost = microposts(:ants)
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(micropost)
+    end
+    assert_redirected_to root_url
   end
 end
